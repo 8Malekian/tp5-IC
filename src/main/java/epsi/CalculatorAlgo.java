e4a0e3b7916a9629ef322f90b850cb3fbe8b6de3
Mathias <naplings@hotmail.com>
2020-12-01 09:23:02 +0100

    add multiply fonction and test

--- src/main/java/epsi/CalculatorAlgo.java
+++ src/main/java/epsi/CalculatorAlgo.java
@@ -15,6 +15,9 @@ public final class CalculatorAlgo {
     public static int add(int a, int b) {
         return a + b;
     }
+    public static int multiply(int a, int b) {
+        return 0;
+    }
 
     private CalculatorAlgo() {
         throw new IllegalArgumentException("Utility class - do not instantiate");
--- src/main/java/epsi/CalculatorService.java
+++ src/main/java/epsi/CalculatorService.java
@@ -17,4 +17,13 @@ public final class CalculatorService {
     public int add(Collection<Integer> numbers) {
         return numbers.stream().reduce(0, CalculatorAlgo::add);
     }
+    /**
+     * Multiply two numbers.
+     * @param a the first number
+     * @param b the second number
+     * @return the result of a * b
+     */
+    public int multiply(Collection<Integer> numbers) {
+        return numbers.stream().reduce(1, CalculatorAlgo::multiply);
+    }
 }
